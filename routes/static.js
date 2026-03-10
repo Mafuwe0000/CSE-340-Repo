cd26a951e37cfee939e6da7960278f108a773cb0
Mafuwe Manuel <mafuwemanuel@gmail.com>
2026-03-10 12:08:11 +0200

    work being done cse340

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); //This line is saying that the express pacage is brought into scope of the file and assigned to a local variable.
+const router = express.Router(); //The Express "router" funcionality is invoked and stored into a local variable for use. the () is indicating that router is a function.
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
-
-module.exports = router;
+router.use(express.static("public")); // in here the Express router is to "use the express.static" function meaning this is where static resources will be found , with the public folder.
+router.use("/css", express.static(__dirname + "public/css")); //this line indicates that any route that contais /css is to refer to the public/css folder, which is found at the root level of the project
+router.use("/js", express.static(__dirname + "public/js")); // and this line indicates that any route that contains /js is to refer to the public/js folder, which is found at te root level of the project
+router.use("/images", express.static(__dirname + "public/images")); //same as the line above
+//Lines 7,8,9 allow us to write paths pointing to these resources easily and also to add subfoldes for images, CSS, and JavaScript and have them still operate correctly
+module.exports = router; //This line exports the router object, along with all these use statements for use in other areas of the application. This is VERY IMPORTANT. if a resource is NOT exported it cannot be used somewhere else.
 
 
 
--- server.js
+++ server.js
@@ -6,14 +6,35 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts") //in here we are telling the app to require express-ejs-layouts so it can be used. Following the simple rule (you have to have something in order to use it)
 const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const app = express() //the assignment of the Express as a functio to the "app" variable is a common practice in a Node application
+const static = require("./routes/static") //route file named static is imported and stored into a static variable
+
+/* ***********************
+ * View engine templates
+ *************************/
+//below we are saying that ejs wil be the view engine for our application.
+app.set("view engine","ejs") //Built into EJS is the understanding tat all views will be stored at views folder and that is where EJS will look for all view files.
+app.use(expressLayouts)
+app.set("layout","./layouts/layouts") //not at views root. This line says that when express ejs layout goes looking for the basic template for a view it wll fing it in layouts folder and the template will be named layout.
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static)//Instead of router.use it is now app.use meaning that the application itself will use this resource. The resource which has been exported in the static file is now being use by the app. THis single lineof code now allows the app to know where the public folder is located and that it and all of its subfolders will be used for the static files.
+
+//by doing things in this manner it allows for all functionality, while allowing the server.js file to remain unclutterd.
+
+//INDEX route
+app.get("/", function(req, res){ //tis line does a numbr of things, app.get-The express application will watch the "get" object, within http request, for a particular route."/" this is route being watched. It indicates the base route of the application or the route which has no specfic resourse requested.
+  //function(req,res){-A javaScript function that takes the request and response objects as parameters}
+  res.render("index", {title:"Home"})
+  //res.render()-The"res" is the responde object while "render()" is an Expess function that wil retrieve the specified view -"index" to be sent back to the browser.
+  //{title:"Home"}- The curly braces are an object (treated like a variable) which holds a name-value pair. This object supplies the value that the "head" partial file expects to receive. The receive object is passed to the view.
+  //the }) righ curly braces closes the function while the right paranteses closes the "get" route.
+})
+
 
 /* ***********************
  * Local Server Information
